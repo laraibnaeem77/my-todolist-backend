a272433bdfcbf382f7e806d2bca0d7c71e6de861
laraibnaeem77 <laraibnaeemawann@gmail.com>
2026-02-23 17:05:11 +0500

    Switch backend to PostgreSQL for Railway

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = 'sqlite:///todos.db'
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
-SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
